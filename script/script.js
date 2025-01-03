478d7383614730dcef1b8d42ddf9a1943c1da975
alfredorndon <alfredorndon@gmail.com>
2025-01-03 10:47:21 -0400

    archivos de jharold actualizados

--- script/script.js
+++ script/script.js
@@ -3,10 +3,12 @@ const filas = 10;
 const columnas = 10;
 const jugadores = 1;
 
-function crearTablero ()
+function crearTablero (jugadores)
 {
     for (let j=1; j<=jugadores; j++)
     {
+        let tableroJuego= document.createElement('div');
+        tableroJuego.setAttribute('class','tablero-juego');
         let tablero = document.createElement('div');
         tablero.setAttribute('class', 'tablero');
         tablero.setAttribute('id', 'tabla-p'+j)
@@ -31,15 +33,192 @@ function crearTablero ()
                 else
                 {
                     let celda = document.createElement('div');
-                    celda.setAttribute('class', 'position table-cell');
                     celda.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
-                    tablero.appendChild(celda);
+                    if (celda.id==='p'+j+'-C1')
+                    {
+                            let celdaBarco= document.createElement('div');
+                            celdaBarco.setAttribute('class', 'position barco-destructor tile-1 horizontal');
+                            celdaBarco.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
+                            tablero.appendChild(celdaBarco);
+                            if (j!=1)
+                                celdaBarco.style.backgroundImage='none';
+                    }
+                    else if (celda.id==='p'+j+'-D1')
+                    {
+                            let celdaBarco= document.createElement('div');
+                            celdaBarco.setAttribute('class', 'position barco-destructor tile-2 horizontal');
+                            celdaBarco.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
+                            tablero.appendChild(celdaBarco);
+                            if (j!=1)
+                                celdaBarco.style.backgroundImage='none';
+                    }
+                    else if (celda.id==='p'+j+'-C3')
+                    {
+                            let celdaBarco= document.createElement('div');
+                            celdaBarco.setAttribute('class', 'position barco-portaaviones tile-1 horizontal');
+                            celdaBarco.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
+                            tablero.appendChild(celdaBarco);
+                            if (j!=1)
+                                celdaBarco.style.backgroundImage='none';
+                    }
+                    else if (celda.id==='p'+j+'-D3')
+                    {
+                            let celdaBarco= document.createElement('div');
+                            celdaBarco.setAttribute('class', 'position barco-portaaviones tile-2 horizontal');
+                            celdaBarco.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
+                            tablero.appendChild(celdaBarco);
+                            if (j!=1)
+                                celdaBarco.style.backgroundImage='none';
+                    }
+                    else if (celda.id==='p'+j+'-E3')
+                    {
+                            let celdaBarco= document.createElement('div');
+                            celdaBarco.setAttribute('class', 'position barco-portaaviones tile-3 horizontal');
+                            celdaBarco.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
+                            tablero.appendChild(celdaBarco);
+                            if (j!=1)
+                                celdaBarco.style.backgroundImage='none';
+                    }
+                    else if (celda.id==='p'+j+'-F3')
+                    {
+                                let celdaBarco= document.createElement('div');
+                                celdaBarco.setAttribute('class', 'position barco-portaaviones tile-4 horizontal');
+                                celdaBarco.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
+                                tablero.appendChild(celdaBarco);
+                                if (j!=1)
+                                    celdaBarco.style.backgroundImage='none';
+                    }
+                    else if (celda.id==='p'+j+'-G3')
+                    {
+                                let celdaBarco= document.createElement('div');
+                                celdaBarco.setAttribute('class', 'position barco-portaaviones tile-5 horizontal');
+                                celdaBarco.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
+                                tablero.appendChild(celdaBarco);
+                                if (j!=1)
+                                    celdaBarco.style.backgroundImage='none';
+                    }
+                    else if (celda.id==='p'+j+'-B5')
+                    {
+                                let celdaBarco= document.createElement('div');
+                                celdaBarco.setAttribute('class', 'position barco-submarino tile-1 vertical');
+                                celdaBarco.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
+                                tablero.appendChild(celdaBarco);
+                                if (j!=1)
+                                    celdaBarco.style.backgroundImage='none';
+                    }
+                    else if (celda.id==='p'+j+'-B6')
+                    {
+                                let celdaBarco= document.createElement('div');
+                                celdaBarco.setAttribute('class', 'position barco-submarino tile-2 vertical');
+                                celdaBarco.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
+                                tablero.appendChild(celdaBarco);
+                                if (j!=1)
+                                celdaBarco.style.backgroundImage='none';
+                    }
+                    else if (celda.id==='p'+j+'-B7')
+                    {
+                                let celdaBarco= document.createElement('div');
+                                celdaBarco.setAttribute('class', 'position barco-submarino tile-3 vertical');
+                                celdaBarco.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
+                                tablero.appendChild(celdaBarco);
+                                if (j!=1)
+                                    celdaBarco.style.backgroundImage='none';
+                    }
+                    else if (celda.id==='p'+j+'-I5')
+                    {
+                                let celdaBarco= document.createElement('div');
+                                celdaBarco.setAttribute('class', 'position barco-crucero tile-1 vertical');
+                                celdaBarco.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
+                                tablero.appendChild(celdaBarco);
+                                if (j!=1)
+                                    celdaBarco.style.backgroundImage='none';
+                    }
+                    else if (celda.id==='p'+j+'-I6')
+                    {
+                                let celdaBarco= document.createElement('div');
+                                celdaBarco.setAttribute('class', 'position barco-crucero tile-2 vertical');
+                                celdaBarco.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
+                                tablero.appendChild(celdaBarco);
+                                if (j!=1)
+                                    celdaBarco.style.backgroundImage='none';
+                    }
+                    else if (celda.id==='p'+j+'-I7')
+                    {
+                                let celdaBarco= document.createElement('div');
+                                celdaBarco.setAttribute('class', 'position barco-crucero tile-3 vertical');
+                                celdaBarco.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
+                                tablero.appendChild(celdaBarco);
+                                if (j!=1)
+                                    celdaBarco.style.backgroundImage='none';
+                    }
+                    else if (celda.id==='p'+j+'-F6')
+                    {
+                                let celdaBarco= document.createElement('div');
+                                celdaBarco.setAttribute('class', 'position barco-acorazado tile-1 vertical');
+                                celdaBarco.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
+                                tablero.appendChild(celdaBarco);
+                                if (j!=1)
+                                    celdaBarco.style.backgroundImage='none';
+                    }
+                    else if (celda.id==='p'+j+'-F7')
+                    {
+                                let celdaBarco= document.createElement('div');
+                                celdaBarco.setAttribute('class', 'position barco-acorazado tile-2 vertical');
+                                celdaBarco.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
+                                tablero.appendChild(celdaBarco);
+                                if (j!=1)
+                                    celdaBarco.style.backgroundImage='none';
+
+                    }
+                    else if (celda.id==='p'+j+'-F8')
+                    {
+                                let celdaBarco= document.createElement('div');
+                                celdaBarco.setAttribute('class', 'position barco-acorazado tile-3 vertical');
+                                celdaBarco.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
+                                tablero.appendChild(celdaBarco);
+                                if (j!=1)
+                                    celdaBarco.style.backgroundImage='none';
+                    }
+                    else if (celda.id==='p'+j+'-F9')
+                        {
+                                let celdaBarco= document.createElement('div');
+                                celdaBarco.setAttribute('class', 'position barco-acorazado tile-4 vertical');
+                                celdaBarco.setAttribute('id', 'p'+j+'-'+abecedario[k]+i);
+                                tablero.appendChild(celdaBarco);
+                                if (j!=1)
+                                celdaBarco.style.backgroundImage='none';
+                        }
+                    else
+                    {
+                        celda.setAttribute('class', 'position table-cell');
+                        tablero.appendChild(celda);
+                    }
                 }
             }
         }
         let section = document.getElementById('tableros');
-        section.prepend(tablero);
+        let titulo= document.createElement('h2');
+        if (j==1)
+        {
+            titulo.setAttribute('class','jugador');
+            titulo.setAttribute('id','p1');
+            titulo.innerText = 'Tu tablero ('+localStorage.getItem('nombreJugador')+')';
+        }
+        else
+        {
+            if (j==2)
+            {
+                let espacio= document.createElement('div');
+                espacio.setAttribute('id', 'espacio-vacio');
+                section.prepend(espacio);
+            }
+            let M=j-1;
+            titulo.setAttribute('class','jugador');
+            titulo.setAttribute('id','p'+j);
+            titulo.innerText = 'Enemigo '+M;
+        }
+        section.prepend(tableroJuego);
+        tableroJuego.appendChild(titulo);
+        tableroJuego.appendChild(tablero);
     }
 }
-
-crearTablero();
